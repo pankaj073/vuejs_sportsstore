95880cd608508d91f2849fa2fa111aab98d50260
Pankaj Sharma <pankaj.sharma1@squareyards.co.in>
2020-06-23 13:25:41 +0530

    Using the RESTful Web Service

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
-  <store />
+	<store />
 </template>
 
 <script>
-import Store from './components/Store'
+	import Store from "./components/Store";
+	import { mapActions } from "vuex";
 
-export default {
-  name: 'App',
-  components: {
-    Store
-  }
-}
+	export default {
+		name: "App",
+		components: {
+			Store
+		},
+		methods: {
+			...mapActions(["getData"])
+		},
+		created() {
+			this.getData();
+		}
+	};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import Axios from "axios";
+
 Vue.use(Vuex);
 
-const testData = [];
-
-for (let i = 1; i <= 10; i++) {
-    testData.push({
-        id: i, 
-        name: `Product #${i}`,
-        category: `Category ${i % 3}`,
-        description: `This is Product #${i}`, price: i * 50
-    });
-}
+const baseUrl = "http://localhost:3500";
+const productsUrl = `${baseUrl}/products`;
+const categoriesUrl = `${baseUrl}/categories`;
 
 export default new Vuex.Store({
     strict: true,
     state: {
-        products: testData,
-        productsTotal: testData.length,
+        products: [],
+        categoriesData: [],
+        productsTotal: 0,
         currentPage: 1,
         pageSize: 4,
         currentCategory: "All"
@@ -32,8 +28,7 @@ export default new Vuex.Store({
                 .slice(index, index + state.pageSize);
         },
         pageCount: (state, getters) => Math.ceil(getters.productsFilteredByCategory.length / state.pageSize),
-        categories: state => ["All",
-            ...new Set(state.products.map(p => p.category).sort())]
+        categories: state => ["All", ...state.categoriesData]
     },
     mutations: {
         setCurrentPage(state, page) {
@@ -46,6 +41,18 @@ export default new Vuex.Store({
         setCurrentCategory(state, category) {
             state.currentCategory = category;
             state.currentPage = 1;
+        },
+        setData(state, data) {
+            state.products = data.pdata;
+            state.productsTotal = data.pdata.length;
+            state.categoriesData = data.cdata.sort();
+        }
+    },
+    actions: {
+        async getData(context) {
+            let pdata = (await Axios.get(productsUrl)).data;
+            let cdata = (await Axios.get(categoriesUrl)).data;
+            context.commit("setData", { pdata, cdata });
         }
     }
 });
